364b10ad86b8057dd92a44295a21480f8f7aea6e
Jarno Walgemoed <j.walgemoed@sourcelabs.nl>
2017-12-04 22:45:19 +0100

    Package added

---
+++ src/main/kotlin/nl/sourcelabs/demo/02-Toplevel.kt
@@ -0,0 +1,21 @@
+package nl.sourcelabs.demo
+/**
+ * Top level constants follow the same convention as previously shown for the hello world example, they
+ * will be compiled into a class with the <source-file-name>Kt, in this case 02-TopLevelKt
+ */
+
+// Top level constant -> public static final String
+const val HELLO = "Hello World"
+
+// Top level value -> private static final String hello. 
+// You also get public static String getHello() for this, as the
+// property is 'public' for Kotlin in this instance; so kotlin defines an accessor.
+val hello = HELLO
+
+// Top level function -> public static final String hello()
+fun hello() = hello
+
+// Main function.
+fun main(args: Array<String>) {
+    println(hello())
+}
---
+++ src/main/kotlin/nl/sourcelabs/demo/08-Lambda.kt
@@ -0,0 +1,29 @@
+package nl.sourcelabs.demo
+
+import java.util.stream.Collectors
+
+fun main(args: Array<String>) {
+    // Consider another list of numbers
+    val numbers = (1..20).toList()
+
+    // This is the java equivalent of filtering a collection
+    val evens = numbers.stream()
+            .filter({ num -> num % 2 == 0 })
+            .collect(Collectors.toList())
+
+    // When using pure kotlin, filtering evens is as easy as this
+    val kotlinEvens = numbers
+            .filter { it % 2 == 0 }
+
+    // Notice the syntax for lambda's. All below have the same result!
+    // long syntax, lambda as parameter and variable declaration
+    numbers.filter({ num -> num % 2 == 0 }) 
+    // Lambda's with one variable can omit the variable declaration, and use 'it' to access it  
+    numbers.filter({ it % 2 == 0 }) 
+    // When the last (or only) parameter of a function is a lambda, you can move it out of the parenthesis
+    numbers.filter() { it % 2 == 0 }
+    // When the above case is true and the parenthesis are empty, you can delete them altogether
+    numbers.filter { it % 2 == 0 }
+    
+    // Compact lambdas!
+}
